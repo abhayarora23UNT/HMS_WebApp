b72e877f6aacebcf2534018220e9c8077d992beb
Dheeraj Reddy Aguthu <dheerajreddyaguthu@my.unt.edu>
2022-03-23 17:19:13 -0500

    login screen

--- HMS/Source Code/Front_End/HmsApp/src/app/modules/authentication/login/login.component.ts
+++ HMS/Source Code/Front_End/HmsApp/src/app/modules/authentication/login/login.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subject, takeUntil } from 'rxjs';
+import { Constants, ModuleConstants } from 'src/app/core/constants/constants';
+import { AuthenticationService } from 'src/app/core/services/Authentication/authentication.service';
+import { Messages } from 'src/app/core/messages/messages';
+import { ToastMessageService } from 'src/app/core/services/utils/toast-message.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit,  OnDestroy {
+  fgLogin!: FormGroup;
+  private onDestroy$: Subject<void> = new Subject<void>();
+  constructor(private formBuilder: FormBuilder, private authService: AuthenticationService, private toastService: ToastMessageService) { 
+    this.createLoginFormGroup();
+  }
 
-  constructor() { }
+  createLoginFormGroup() {
+    this.fgLogin = this.formBuilder.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
+  }
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+  }
+
+  loginUser(){
+    if (this.fgLogin.status == Constants.FormInvalid) {
+      this.toastService.errorMessage(Messages.Mandatory_Fields_Validation);
+    }
   }
 
 }
