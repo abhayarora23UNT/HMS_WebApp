c5a594258804a004ffce9b21f1d4b7241d446c23
Abhay Arora <AbhayArora@my.unt.edu>
2022-04-02 01:03:43 -0500

    ts file added

---
+++ HMS/Source Code/Front_End/HmsApp/src/app/modules/admin/hospital-branch/add-hospital-branch/add-hospital-branch-component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-hospital-branch',
+  templateUrl: './add-hospital-branch.component.html',
+  styleUrls: ['./add-hospital-branch.component.scss']
+})
+export class AddHospitalBranchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log('123');
+  }
+
+}
