f73c6f6271e1627d109fe1a4b9cc5ece0f8426bb
Abhay Arora <AbhayArora@my.unt.edu>
2022-04-08 12:16:01 -0500

    list doctor implementation

--- HMS/Source Code/Front_End/HmsApp/src/app/modules/admin/doctor/list-doctor/list-doctor.component.ts
+++ HMS/Source Code/Front_End/HmsApp/src/app/modules/admin/doctor/list-doctor/list-doctor.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { LookupService } from 'src/app/core/services/lookups/lookups.service';
+import { ToastMessageService } from 'src/app/core/services/utils/toast-message.service';
+import { Doctor } from 'src/app/shared/models/doctor/doctor-resp-data';
 
 @Component({
   selector: 'app-list-doctor',
@@ -10,10 +14,17 @@ export class ListDoctorComponent implements OnInit {
   appointmentColumns: string[] = ['name', 'designation', 'mobileno', 'gender', 'email'];
   isDataLoading = false; // flag to hide/show loader
   dataSource: any = []; 
-  constructor( private router: Router) { }
+  private onDestroy$: Subject<void> = new Subject<void>();
+  constructor(private router: Router, private lookupService: LookupService, private toastService: ToastMessageService) { }
 
   ngOnInit(): void {
+    this.getDoctorsList();
   }
+  
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+  }
+
   /**
    * Method to navigate to edit appointment page
    * @param event 
@@ -39,5 +50,58 @@ export class ListDoctorComponent implements OnInit {
     this.router.navigate(["admin/dashboard/addDoctor"]);
   }
 
+  
+  /**
+   * Method to get appointment list
+   */
+   getDoctorsList() {
+    this.isDataLoading = true;
+    this.dataSource = [];
+    this.lookupService.getDoctorsList()
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe({
+        next: (retData: any) => {
+          if (retData.status) {
+            if (retData.data.Table != null && retData.data.Table.length > 0) {
+              this.parseListResponse(retData);
+            }
+          } else {
+            this.toastService.errorMessage(retData.message);
+          }
+          this.isDataLoading = false;
+        },
+        error: (err: any) => {
+          console.log(err);
+          this.isDataLoading = false;
+        },
+        complete: () => {
+          this.isDataLoading = false;
+        }
+      });
+  }
+
+  /**
+   * Method to parse appointment list response
+   * @param retData 
+   */
+  parseListResponse(retData: any) {
+    const respObjLst = [];
+    for (const row of retData.data.Table) {
+      const respObj = new Doctor();
+      respObj.doctorId = row.DoctorId;
+      respObj.gender = row.Gender;
+      respObj.name = row.Name;
+      respObj.address1 = row.Address1;
+      respObj.address2 = row.Address2;
+      respObj.city = row.City;
+      respObj.phone = row.Phone;
+      respObj.mobile = row.Mobile;
+      respObj.salary = row.Salary;
+      respObj.email = row.Email;
+      respObj.designation = row.Designation;
+      respObjLst.push(respObj);
+    }
+    this.dataSource = respObjLst;
+  }
 
 }
