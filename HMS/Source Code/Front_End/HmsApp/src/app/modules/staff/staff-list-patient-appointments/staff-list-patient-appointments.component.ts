4f2b39d3c4cd59e04024b8c3a41c2dd5e0f09465
Abhay Arora <AbhayArora@my.unt.edu>
2022-04-23 00:49:02 -0500

    Del 5- staff module - initial architecture created

---
+++ HMS/Source Code/Front_End/HmsApp/src/app/modules/staff/staff-list-patient-appointments/staff-list-patient-appointments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-list-patient-appointments',
+  templateUrl: './staff-list-patient-appointments.component.html',
+  styleUrls: ['./staff-list-patient-appointments.component.scss']
+})
+export class StaffListPatientAppointmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ HMS/Source Code/Front_End/HmsApp/src/app/modules/staff/staff-list-patient-medicines/staff-list-patient-medicines.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-list-patient-medicines',
+  templateUrl: './staff-list-patient-medicines.component.html',
+  styleUrls: ['./staff-list-patient-medicines.component.scss']
+})
+export class StaffListPatientMedicinesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
