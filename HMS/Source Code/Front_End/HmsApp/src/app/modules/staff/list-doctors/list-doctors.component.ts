58ffc3df4c10939e53df6246e7a4054c826a473a
Dheeraj Reddy Aguthu <dheerajreddyaguthu@my.unt.edu>
2022-04-28 00:02:28 -0500

    Staff- list doctor changes

--- HMS/Source Code/Front_End/HmsApp/src/app/modules/staff/list-doctors/list-doctors.component.ts
+++ HMS/Source Code/Front_End/HmsApp/src/app/modules/staff/list-doctors/list-doctors.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Subject, takeUntil } from 'rxjs';
+import { NavigationExtras,Router } from '@angular/router';
+import { LookupService } from 'src/app/core/services/lookups/lookups.service';
+import { ToastMessageService } from 'src/app/core/services/utils/toast-message.service';
+import { AdminDoctorService } from 'src/app/core/services/admin/admin-doctor.service';
+import { Doctor } from 'src/app/shared/models/doctor/doctor-resp-data';
+import { Messages } from 'src/app/core/messages/messages';
+import { ConfirmDialogComponent } from 'src/app/shared/components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-staff-list-doctors',
@@ -7,9 +16,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListDoctorsComponent implements OnInit {
 
-  constructor() { }
+  appointmentColumns: string[] = ['name', 'designation', 'phone', 'gender', 'email'];
+  isDataLoading = false; // flag to hide/show loader
+  dataSource: any = []; 
+  private onDestroy$: Subject<void> = new Subject<void>();
+  constructor(private router: Router, private doctorService: AdminDoctorService, private lookupService: LookupService, private toastService: ToastMessageService,private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.getDoctorsList();
+  }
+  
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+  }
+
+
+  /**
+   * Method to get appointment list
+   */
+   getDoctorsList() {
+    this.isDataLoading = true;
+    this.dataSource = [];
+    this.doctorService.getDoctorsList('')
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe({
+        next: (retData: any) => {
+          if (retData.status) {
+            if (retData.data.Table != null && retData.data.Table.length > 0) {
+              this.parseListResponse(retData);
+            }
+          } else {
+            this.toastService.errorMessage(retData.message);
+          }
+          this.isDataLoading = false;
+        },
+        error: (err: any) => {
+          console.log(err);
+          this.isDataLoading = false;
+        },
+        complete: () => {
+          this.isDataLoading = false;
+        }
+      });
+  }
+
+  /**
+   * Method to parse appointment list response
+   * @param retData 
+   */
+  parseListResponse(retData: any) {
+    const respObjLst = [];
+    for (const row of retData.data.Table) {
+      const respObj = new Doctor();
+      respObj.doctorId = row.DoctorId;
+      respObj.gender = row.Gender;
+      respObj.name = row.Name;
+      respObj.address1 = row.Address1;
+      respObj.address2 = row.Address2;
+      respObj.city = row.City;
+      respObj.phone = row.Phone;
+      respObj.mobile = row.Mobile;
+      respObj.salary = row.Salary;
+      respObj.email = row.Email;
+      respObj.designation = row.Designation;
+      respObjLst.push(respObj);
+    }
+    this.dataSource = respObjLst;
   }
 
 }
